bca9d8fec829a7f8cef13f5cb8b8f54794ea051d
IGICOCORO <nduwimanaguymich@gmail.com>
2023-02-12 23:09:03 +0200

    generating schemas

--- lib/collection/product.dart
+++ lib/collection/product.dart
@@ -1,9 +1,13 @@
 import 'package:isar/isar.dart';
+import 'category.dart';
+part 'product.g.dart';
 
 @Collection()
 class Product {
   Id? id =Isar.autoIncrement;
   late String name;
-  late String category;
+  @Index(composite: [CompositeIndex('name')])
+  final category = IsarLink<Category>();
+  late float price;
   late int quantity;
 }
