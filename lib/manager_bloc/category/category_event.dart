9a410a31d08f5748718f02566fb6f08a933456b8
IGICOCORO <nduwimanaguymich@gmail.com>
2023-02-24 13:31:54 +0200

    created category_bloc

--- lib/manager_bloc/category/category_event.dart
+++ lib/manager_bloc/category/category_event.dart
@@ -1,4 +1,18 @@
 part of 'category_bloc.dart';
 
 @immutable
-class CategoryEvent {}
+class CategoryEvent extends Equatable {
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+}
+
+class GetCategories extends CategoryEvent {
+  GetCategories({
+    required this.idSelected,
+  });
+  final int idSelected;
+
+  @override
+  List<Object?> get props => [idSelected];
+}
--- lib/manager_bloc/category/category_state.dart
+++ lib/manager_bloc/category/category_state.dart
@@ -1,6 +1,10 @@
 part of 'category_bloc.dart';
 
 @immutable
-class CategoryState {}
+class CategoryState extends Equatable {
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+}
 
 class CategoryInitial extends CategoryState {}
